c87b47ab3097ac7f849e6d4abf0c3bb90bec4bf3
Kailash Joshi <kailashrajjoshi@gmail.com>
2018-02-27 01:56:06 -0500

    git reset state bug

--- states/gitreset/files/gitreset.sh
+++ states/gitreset/files/gitreset.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ `git status --porcelain` ]]; then
+salt -L "master.local" beacons.disable
   git reset --hard origin
   git clean -f
+sleep 3
+salt -L "master.local" beacons.enable
 else
   echo "no change"
 fi
