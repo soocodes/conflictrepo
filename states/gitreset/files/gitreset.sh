4373e784407f3abd8ec21afa5923eb7b318ffca9
Kailash Joshi <kailashrajjoshi@gmail.com>
2018-02-27 07:37:07 -0500

    Update gitreset.sh

--- states/gitreset/files/gitreset.sh
+++ states/gitreset/files/gitreset.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ `git status --porcelain` ]]; then
-  salt-call beacons.disable
+  salt -L "master.local" beacons.disable
   git reset --hard origin
   git clean -f
   sleep 3
-  salt-call beacons.enable
+  salt -L "master.local" beacons.enable
 else
   echo "no change"
 fi
